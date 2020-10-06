77816f015d4ddfe82ebee1e30b32a5c2b0954457
Guillaume <guillaume-fe@outlook.com>
2020-10-06 14:03:57 +0200

    Update bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -408,16 +408,65 @@ function startRepl (service: Register, state: EvalState, code?: string) {
 
     // Hard fix for TypeScript forcing `Object.defineProperty(exports, ...)`.
     exec('exports = module.exports', state.path)
-    process.stdin.push(".load env\n")
-    process.stdout.write('\u001B[2J\u001B[0;0f');
-    process.stdin.push("\n")
-    process.stdout.write('Welcome to geb-console!\n\n');
-    process.stdout.write('Context objects:\n');
-    process.stdout.write('- geb                   - ethers\n');
-    process.stdout.write('- gebAdmin              - wallet\n');
-    process.stdout.write('- contracts             - provider\n');
-    process.stdout.write('- BigNumber             - wait(promise)\n');
-    process.stdout.write('- ETH_A                 - WAD\n');
+        let file = `
+        import { ethers, Wallet } from 'ethers'
+import {
+    Geb,
+    GebAdmin,
+    utils,
+    contracts,
+    adminContracts,
+} from '@reflexer-finance/geb-admin'
+
+declare var _: any
+
+let rpcUrl: string
+let network: 'kovan' | 'mainnet'
+
+if (process.env.NETWORK === 'mainnet') {
+    console.log('Start geb-console on Mainnet...')
+    rpcUrl = 'https://parity0.mainnet.makerfoundation.com:8545'
+    network = 'mainnet'
+} else {
+    console.log('Start geb-console on Kovan...')
+    rpcUrl = 'https://parity0.kovan.makerfoundation.com:8545'
+    network = 'kovan'
+}
+
+if (process.env.RPC_URL) {
+    rpcUrl = process.env.RPC_URL
+}
+
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+const geb = new Geb(network, provider)
+const gebAdmin = new GebAdmin(network, provider)
+const ETH_A = utils.ETH_A
+const WAD = utils.WAD
+const decodeChainError = utils.decodeChainError
+const BigNumber = ethers.BigNumber
+
+let wallet: Wallet
+if (process.env.PK) {
+    wallet = new ethers.Wallet(process.env.PK, provider)
+}
+
+const wait = <T>(promise: Promise<T>): T => {
+    return require('deasync2').await(promise)
+}
+
+`
+
+        file.split("\n").map(l => {
+            process.stdin.push(l + "\n")
+            process.stdout.write('\u001B[2J\u001B[0;0f');
+        })
+        process.stdout.write('Welcome to geb-console!\n\n');
+        process.stdout.write('Context objects:\n');
+        process.stdout.write('- geb                   - ethers\n');
+        process.stdout.write('- gebAdmin              - wallet\n');
+        process.stdout.write('- contracts             - provider\n');
+        process.stdout.write('- BigNumber             - wait(promise)\n');
+        process.stdout.write('- ETH_A                 - WAD\n');
   }
 
   reset()
